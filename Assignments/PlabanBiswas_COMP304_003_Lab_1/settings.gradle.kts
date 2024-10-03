cfb1ca3c8e9c6f6a40e77546b19a12b2fde65444
Plaban Biswas <plabanbiswas96@outlook.com>
2024-10-03 00:33:13 -0400

    -ADDED: Folder Skeleton with Assignment - 1

---
+++ Assignments/PlabanBiswas_COMP304_003_Lab_1/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PlabanBiswas_COMP304_003_Lab_1"
+include(":app")
